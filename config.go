de2e6d29c20e23320fb66acd402216c0f710b21a
Mike Napolitano <mnapolit@cisco.com>
2017-12-02 00:26:27 -0500

    Utils Folder

--- config.go
+++ config.go
@@ -19,15 +19,6 @@ type Config struct {
 	Cmd            *cobra.Command
 }
 
-func contains(s []string, str string) bool {
-	for _, si := range s {
-		if si == str {
-			return true
-		}
-	}
-	return false
-}
-
 // New creates a config parser using a provided cfg struct.
 // name, desc are for the help window.
 func New(name string, desc string, cfg interface{}) (*Config, error) {
@@ -251,69 +242,6 @@ func eachSubField(i interface{}, fn func(reflect.Value, string, []string) error,
 		}
 	}
 	return nil
-}
-
-func flagString(parent, field string) string {
-	if len(parent) == 0 {
-		return strings.ToLower(hyphen(field))
-	}
-	if len(field) == 0 {
-		return strings.ToLower(hyphen(parent))
-	}
-	return strings.ToLower(hyphen(parent) + "-" + hyphen(field))
-}
-
-func envString(parent, field string) string {
-	if len(parent) == 0 {
-		return strings.ToUpper(underscore(field))
-	}
-	if len(field) == 0 {
-		return strings.ToUpper(underscore(parent))
-	}
-	return strings.ToUpper(underscore(parent) + "_" + underscore(field))
-}
-
-func isUpper(c byte) bool {
-	return c >= 'A' && c <= 'Z'
-}
-
-func isLower(c byte) bool {
-	return !isUpper(c)
-}
-
-func toUpper(c byte) byte {
-	return c - ('a' - 'A')
-}
-
-func toLower(c byte) byte {
-	return c + ('a' - 'A')
-}
-
-// Underscore converts "CamelCasedString" to "camel_cased_string".
-func underscore(s string) string {
-	return splitCamel(s, '_')
-}
-
-// Underscore converts "CamelCasedString" to "camel-cased-string".
-func hyphen(s string) string {
-	return splitCamel(s, '-')
-}
-
-func splitCamel(s string, sep byte) string {
-	r := make([]byte, 0, len(s))
-	for i := 0; i < len(s); i++ {
-		c := s[i]
-		if isUpper(c) {
-			if i > 0 && i+1 < len(s) && (isLower(s[i-1]) || isLower(s[i+1])) {
-				r = append(r, sep, toLower(c))
-			} else {
-				r = append(r, toLower(c))
-			}
-		} else {
-			r = append(r, c)
-		}
-	}
-	return string(r)
 }
 
 func checkRequiredFlags(flags *pflag.FlagSet) error {
